f176eabea5c08263ba95cba64d1fe72af2cf7914
jstrieb/github-stats <vadnetvadnet@ukr.net>
2024-09-03 18:52:22 +0300

    Update urls.py

--- app/comments/urls.py
+++ app/comments/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import CommentViewSet
 
 router = DefaultRouter()
-router.register(r'comments', CommentViewSet)
+router.register(r"comments", CommentViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path("api/", include(router.urls)),
 ]
