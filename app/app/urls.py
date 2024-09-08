f9237869c2b6d5329c222172303af82efe7cf78b
jstrieb/github-stats <vadnetvadnet@ukr.net>
2024-09-08 19:25:20 +0300

    commit

--- app/app/urls.py
+++ app/app/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from comments.routing import websocket_urlpatterns
 from comments.urls import router
 
 
@@ -13,5 +14,5 @@ urlpatterns = [
     path(
         "api/v1/api-auth/", include("rest_framework.urls", namespace="rest_framework")
     ),
-    # path("ws/", include(websocket_urlpatterns)),
+    path("ws/", include(websocket_urlpatterns)),
 ]
